7ae3bd5def8186c07f76eeea1cfde2c1d447fb89
Janani-06 <pjjanani1997@gmail.com>
2020-11-21 22:39:18 +0530

    Initial project

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,15 @@
+package org.add;
+
+public class GreensTech {
+	private void greensomr() {
+System.out.println("omr");
+	}
+private void greensadayar() {
+System.out.println("adayar");
+}
+public static void main(String[] args) {
+	GreensTech i = new GreensTech();
+	i.greensomr();
+	
+}
+}
